55f424625308898a77df1035e683f44d24ad4d6c
LinaSunden <sundenlina@gmail.com>
2023-10-26 11:21:35 +0200

    - Made the toplist 4-10 dynamic -structure the topList.js file with regions  and summarys to the functions  -made a function that combine the result from the two apis.

--- js/topList.js
+++ js/topList.js
@@ -1,7 +1,12 @@
+// #region Api calls
+
 const cmdbUrl = "https://grupp6.dsvkurs.miun.se/api";
 const omdbURL = "http://www.omdbapi.com/?";
 
-
+/**
+ * 
+ * @returns {Promise<string>} The API key for the OMDB API.
+ */
 async function getApiKey(){
     const endpoint = "/keys/grupp7/46bc07e8-d9d7-4078-8516-e544d35e21e7";
     const response = await fetch(cmdbUrl + endpoint);
@@ -9,6 +14,11 @@ async function getApiKey(){
     return data.apiKey;
 }
 
+/**
+ * 
+ * @param {*} moviesPerPage is the number of movies to be returned from the CMDB API.
+ * @returns return the top movies from the CMDB API.
+ */
 async function getMoviesCmdb(moviesPerPage){
  
 const response = await fetch(`https://grupp6.dsvkurs.miun.se/api/toplists?sort=DESC&limit=${moviesPerPage}&page=1&countLimit=2`);
@@ -16,6 +26,11 @@ const response = await fetch(`https://grupp6.dsvkurs.miun.se/api/toplists?sort=D
     return movies;
 }
 
+/**
+ * 
+ * @param {*} imdbID 
+ * @returns fetch the movie with the specifik imdbID from the OMDB API.
+ */
 async function getMovieOmdb(imdbID){
     const response = await fetch(omdbURL + new URLSearchParams({
         i: `${imdbID}`,
@@ -23,50 +38,54 @@ async function getMovieOmdb(imdbID){
     }));
     
     const oneMovie = await response.json();
-    //displayMovieList(oneMovie);
     return oneMovie;
 }
 
-async function combineResults(movies , oneMovie){
-    const a = movies.movies;
-    const b = oneMovie;
-    const result = a.push(b);
-    return result;
+// #endregion
+
+
+/**
+ * Take the results from the CMDB and the OMDB API and combine them into one object.
+ * @returns {Promise<Array>} An array of combined movie objects.
+ */
+async function combineResults(){
+    const moviesCmdb = await getMoviesCmdb(10);
+    const moviePromises = moviesCmdb.movies.map(async (movie) => {
+        const imdbID = movie.imdbID;
+        const movieOmdb = await getMovieOmdb(imdbID);
+        return { ...movie, ...movieOmdb };
+    });
+
+    const combinedMovies = await Promise.all(moviePromises);
+    return combinedMovies;
 }
 
-async function fetchMovies(){
+// #region Fetch top movies
+
+/**
+ * Fetch the top 3 movies from the CMDB with data from OMDB API.
+ */
+async function fetchMoviestop3(){
 const top1Container = document.getElementById('top1');
 const top2Container = document.getElementById('top2');
 const top3Container = document.getElementById('top3');
-const top4TContainer = document.getElementById('top4');
-const top5Container = document.getElementById('top5');
-const top6Container = document.getElementById('top6');
-const top7Container = document.getElementById('top7');
-const top8Container = document.getElementById('top8');
-const top9Container = document.getElementById('top9');
-const top10Container = document.getElementById('top10');
 
 try {
-    const movies = await getMoviesCmdb(10);
-
-    movies.movies.forEach(async (movie, index) => {
-      const imdbID = movie.imdbID;
-      const oneMovie = await getMovieOmdb(imdbID);
+    const combinedMovies = await combineResults(); 
     
-      const movielist = await combineResults(movies, oneMovie);
-    console.log(movielist);
+    combinedMovies.slice(0, 3).forEach(async (movie, index) => {
 
       const movieContainer = document.createElement('div');
       movieContainer.classList.add('movie-container');
 
       const movieTitle = document.createElement('h3');
       movieTitle.classList.add('movie-title');
-      movieTitle.textContent = `${oneMovie.Title}`;
+      movieTitle.textContent = `${movie.Title}`;
 
       const movieImg = document.createElement('img');
       movieImg.classList.add('movie-img');
-      movieImg.src = `${oneMovie.Poster}`;
-      movieImg.alt = `Poster of ${oneMovie.Title}`;
+      movieImg.src = `${movie.Poster}`;
+      movieImg.alt = `Poster of ${movie.Title}`;
 
       const summary = document.createElement('div');
       summary.classList.add('summary');
@@ -94,7 +113,7 @@ try {
 
       const moviePlot = document.createElement('p');
       moviePlot.classList.add('movie-plot');
-      moviePlot.textContent = `${oneMovie.Plot}`;
+      moviePlot.textContent = `${movie.Plot}`;
 
       const readMoreButton = document.createElement('button');
       readMoreButton.classList.add('read-more-button');
@@ -127,27 +146,6 @@ switch (index) {
     case 2:
         top3Container.appendChild(movieContainer);
         break;
-    case 3:
-        top4TContainer.appendChild(movieContainer);
-        break;
-    case 4:
-        top5Container.appendChild(movieContainer);
-        break;
-    case 5:
-        top6Container.appendChild(movieContainer);
-        break;
-    case 6:
-        top7Container.appendChild(movieContainer);
-        break;
-    case 7:
-        top8Container.appendChild(movieContainer);
-        break;
-    case 8:
-        top9Container.appendChild(movieContainer);
-        break;
-    case 9:
-        top10Container.appendChild(movieContainer);
-        break;
     default:    
         break;  
     }
@@ -159,7 +157,112 @@ switch (index) {
   }
 }
 
+fetchMoviestop3();
 
+/**
+ * Fetch the top 4-10 movies from the CMDB with data from OMDB API.
+ */
+async function fetchMoviesFromTop4() {
+    const topContainers = [];
+
+    for (let i = 4; i <= 10; i++) {
+      const topContainer = document.createElement('div');
+      topContainer.classList.add('flex-item-4-10');
+      topContainer.id = `top${i}`;
+  
+      const rankElement = document.createElement('h1');
+      rankElement.id = `rank${i}`;
+      rankElement.textContent = i;
+  
+      topContainer.appendChild(rankElement);
+      topContainers.push(topContainer);
+    }
+
+    const top4To10Container = document.querySelector('.top4-10-container');
+    topContainers.forEach(container => top4To10Container.appendChild(container));
+  
+    try {
+      const combinedMovies = await combineResults();
+  
+      combinedMovies.slice(3, 3 + topContainers.length).forEach(async (movie, index) => {
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+  
+        const movieTitle = document.createElement('h3');
+        movieTitle.classList.add('movie-title');
+        movieTitle.textContent = `${movie.Title}`;
+  
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.src = `${movie.Poster}`;
+        movieImg.alt = `Poster of ${movie.Title}`;
+  
+        const summary = document.createElement('div');
+        summary.classList.add('summary');
+  
+        const movieScore = document.createElement('p');
+        movieScore.classList.add('movie-score');
+        movieScore.textContent = `Score: ${movie.cmdbScore}`;
+  
+        const setRating = document.createElement('span');
+          setRating.classList.add('set-rating');
+          setRating.textContent = 'Score movie';
+          
+          const rating = document.createElement('div');
+          rating.classList.add('rating');
+          const ratingOptions = ['1', '2', '3', '4'];
+          ratingOptions.forEach(option => {
+              const listItem = document.createElement('li');
+              const link = document.createElement('a');
+              link.href = '#';
+              link.classList.add("_"+ option);
+              link.textContent = option;
+              listItem.appendChild(link);
+              rating.appendChild(listItem);
+            });
+    
+          const moviePlot = document.createElement('p');
+          moviePlot.classList.add('movie-plot');
+          moviePlot.textContent = `${movie.Plot}`;
+    
+          const readMoreButton = document.createElement('button');
+          readMoreButton.classList.add('read-more-button');
+          readMoreButton.textContent = 'Read more...';
+    
+          readMoreToggler(readMoreButton, moviePlot);
+    
+          const toMovieDetails = document.createElement('button');
+            toMovieDetails.classList.add('to-movie-details');
+            toMovieDetails.textContent = 'To movie details';
+    
+          summary.appendChild(movieScore);
+          summary.appendChild(setRating);
+          summary.appendChild(rating);
+          summary.appendChild(moviePlot);
+          summary.appendChild(readMoreButton);
+    
+          movieContainer.appendChild(movieTitle);
+          movieContainer.appendChild(movieImg);
+          movieContainer.appendChild(summary);
+          movieContainer.appendChild(toMovieDetails);
+  
+        if (index < topContainers.length) {
+          topContainers[index].appendChild(movieContainer);
+        }
+      });
+    } catch (error) {
+      handleError(error);
+    }
+  }
+
+  fetchMoviesFromTop4();
+
+
+  /**
+ * Create a toggle function for the read more button.
+ * @param {*} readMoreButton 
+ * @param {*} moviePlot 
+ */
 function readMoreToggler (readMoreButton, moviePlot) {
     readMoreButton.addEventListener('click', () => {
         moviePlot.classList.toggle('expanded');
@@ -171,67 +274,5 @@ function readMoreToggler (readMoreButton, moviePlot) {
     });
 }
 
-fetchMovies();
-
-
-function handleError(error)
-{
-    console.log(error);
-}
-
-/*
-
-async function displayMovieList(movie)
-{
-   const top1 = document.querySelector('.movieImg1'); 
-   top1.src = `${movie.Poster}`;
-
-   const movieTitle1 = document.getElementById('movieTitle1');
-   movieTitle1.textContent =`${movie.Title}`;
-
-   const plot = document.getElementById('plotTop1');
-   plot.textContent =`${movie.Plot}`;
-
-   console.log(movie); 
-}
-
-
-
-
-
-
-async function fetchMovies(){
-
-    try {
-        const movies = await getMoviesCmdb(10);
-        //const present = await getMovieOmdb();
-        console.log(movies);
-       
-        const firstMovie = movies.movies[0];
-        console.log(firstMovie);
-
-        const imdbID = firstMovie.imdbID;
-        const scoreTop1 = document.getElementById('scoreMovie1');
-        scoreTop1.textContent = `Betyg ${firstMovie.cmdbScore}`;
-        getMovieOmdb(imdbID);
-        
-     
-      
-        
-        
-    } catch (error) {
-        handleError(error);
-    }
-   
-};
-
-
-
-fetchMovies();
-
-
-//https://www.omdbapi.com/?i=tt6166392&apikey=5a4be969
-
-*/
-
+  // #endregion
 
