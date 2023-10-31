bd5f57a611ab89bc2cfe46889c8869f9280b64be
moma97 <moniramah@gmail.com>
2023-10-31 01:38:19 +0100

    clean

--- js/postReview.js
+++ js/postReview.js
@@ -1,56 +1,41 @@
-
-// import { movieInfo } from './movieDetails.js';
-// import { showReviews } from "./movieDetails";
 import { ratedMovies } from './scoreRate.js';
 
-// showReviews();
+    const imdbID = getImdbIDFromURL();
+    console.log('IMDb ID:', imdbID);
 
-
-// console.log('Before submitReviewToAPI');
-// console.log('After submitReviewToAPI');
-const imdbID = getImdbIDFromURL();
-console.log('IMDb ID:', imdbID);
-
- function getImdbIDFromURL() {
+    function getImdbIDFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get('imdbID');
-  }
+    }
 
-
-// if (ratedMovies.includes(imdbID)) {
-    
-//     const userRating = getRatingForMovie(imdbID);
-    
-//     const userRatingInfo = document.getElementById('user-rating-info');
-//     userRatingInfo.textContent = 'You have already rated this movie';
-//     userRatingInfo.style.display = 'block';
-// }
+        const buttonReview2 = document.getElementById('button-review2');
 
 
 
+        buttonReview2.addEventListener('click', function(event) {
+        event.preventDefault();
+
+       
+           
+            const firstName = document.getElementById("firstName").value;
+            const lastName = document.getElementById("lastName").value;
+            const fullName = `${firstName} ${lastName}`;
+            const date = document.getElementById("date").value;
+            const review = document.getElementById("review").value;
+            const score = 3;
+            if (!firstName || !lastName || !date || !review) {
+                alert('Please fill in all fields'); 
+                return;
+            } else {
+                submitReviewToAPI(imdbID, fullName, score, review, date);
+            }
+           
+      
+    });
 
 
+    async function submitReviewToAPI(imdbID, fullName, score, review, date){
 
-
- const buttonReview2 = document.getElementById('button-review2');
-
- buttonReview2.addEventListener('click', function(event) {
-    event.preventDefault();
-
-     // Get form inputs
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const fullName = `${firstName} ${lastName}`;
-    const date = document.getElementById("date").value;
-    const review = document.getElementById("review").value;
-    const score = 3;
-
-    submitReviewToAPI(imdbID, fullName, score, review, date);
-});
-
-
- async function submitReviewToAPI(imdbID, fullName, score, review, date){
-    
     const apiUrl = `https://grupp6.dsvkurs.miun.se/api/movies/review`;
 
     const reviewData = {
@@ -88,48 +73,9 @@ console.log('IMDb ID:', imdbID);
 }
 
      
-    
-
-
-
-//    async function submitReview(event) {
-//     console.log('Button clicked')
-//     event.preventDefault(); 
-
-
-
-    // Get form inputs
-    // const firstName = document.getElementById("firstName").value;
-    // const lastName = document.getElementById("lastName").value;
-    // const date = document.getElementById("date").value;
-    // const reviewText = document.getElementById("review").value;
-
-    // // Validate inputs
-    // if (!firstName || !lastName || !date || !reviewText) {
-    //     alert('Please fill in all fields'); // Display an alert if any field is empty
-    //     return;
-    // } 
+ 
+   
 
 //automatiskt lägga till dagens datum 
 
-    // Prepare review data
-    // const reviewData = {
-    //     imdbID: imdbID,
-    //     reviewer: `${firstName} ${lastName}`,
-    //     score: 3,
-    //     review: reviewText,
-    //     date: date,
-    // };
-    // console.log(reviewData);
 
-    
-    
-
-    // Clear form inputs
-    // document.getElementById('firstName').value = '';
-    // document.getElementById('lastName').value = '';
-    // document.getElementById('date').value = '';
-    // document.getElementById('review').value = '';
-// }
-
-// export { submitReview };
